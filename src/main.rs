200144e17be76ef0798ba8f06257ef799a251538
iliangood <ilia191168@gmail.com>
2026-02-27 20:59:25 +0300

    Добавил возможность мемоизации

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,29 @@ use std::{
     env,
     io::{self, Write},
 };
-fn factorial(num: u64) -> BigInt {
+
+struct MemFactorial {
+    facts: Vec<BigInt>,
+}
+impl MemFactorial {
+    fn new() -> MemFactorial {
+        MemFactorial {
+            facts: vec![BigInt::from(1), BigInt::from(1)],
+        }
+    }
+    fn calc(&mut self, num: usize) -> BigInt {
+        if self.facts.len() > num {
+            return self.facts[num].clone();
+        }
+        for i in self.facts.len()..=num {
+            self.facts
+                .push(self.facts.last().unwrap().clone() * BigInt::from(i));
+        }
+        self.facts.last().unwrap().clone()
+    }
+}
+
+fn multi_factorial(num: usize) -> BigInt {
     if num < 2 {
         return BigInt::from(1);
     }
@@ -16,9 +38,30 @@ fn factorial(num: u64) -> BigInt {
         .map(BigInt::from)
         .reduce(|| BigInt::from(1), |acc, cur| acc * cur)
 }
-fn combinations(base: u64, subset: u64) -> BigInt {
-    factorial(base) / factorial(base - subset) / factorial(subset)
+
+struct MemCombinations {
+    fact_calc: MemFactorial,
 }
+impl MemCombinations {
+    fn new() -> MemCombinations {
+        MemCombinations {
+            fact_calc: MemFactorial::new(),
+        }
+    }
+    fn calc(&mut self, base: usize, subset: usize) -> BigInt {
+        if base < 100000 && subset < 100000 {
+            self.fact_calc.calc(base)
+                / self.fact_calc.calc(base - subset)
+                / self.fact_calc.calc(subset)
+        } else {
+            multi_factorial(base) / multi_factorial(base - subset) / multi_factorial(subset)
+        }
+    }
+}
+
+// fn combinations(base: usize, subset: usize) -> BigInt {
+//     multi_factorial(base) / multi_factorial(base - subset) / multi_factorial(subset)
+// }
 fn main() {
     let mut use_float = false;
     for arg in env::args().skip(1) {
@@ -29,13 +72,14 @@ fn main() {
             }
         }
     }
+    let mut mem_comb = MemCombinations::new();
     let mut input = String::new();
     let n = loop {
         print!("enter n:");
         io::stdout().flush().expect("output error");
         io::stdin().read_line(&mut input).expect("input error");
         let num_s = input.trim();
-        match num_s.parse::<u64>() {
+        match num_s.parse::<usize>() {
             Err(_) => println!("{} isn't a positive number", num_s),
             Ok(0) => println!("{} is zero, number should be positive", 0),
             Ok(num) => {
@@ -71,7 +115,7 @@ fn main() {
             println!(
                 "{}:{}",
                 i,
-                (BigRational::from(combinations(n, i))
+                (BigRational::from(mem_comb.calc(n, i))
                     * p.clone().pow(i as i32)
                     * q.clone().pow((n - i) as i32))
                 .to_f64()
@@ -83,7 +127,7 @@ fn main() {
             println!(
                 "{}:{}",
                 i,
-                BigRational::from(combinations(n, i))
+                BigRational::from(mem_comb.calc(n, i))
                     * p.clone().pow(i as i32)
                     * q.clone().pow((n - i) as i32)
             );
