f7eecebb19dcaecd867186d2af1896953026efb1
iliangood <ilia191168@gmail.com>
2026-03-01 20:07:12 +0300

    Работа над оптимизацией вычислений

--- src/main.rs
+++ src/main.rs
@@ -5,55 +5,119 @@ use num_traits::ToPrimitive;
 
 use rayon::prelude::*;
 use std::{
-    env,
+    cmp, env,
     io::{self, Write},
+    thread::{self, JoinHandle},
 };
 
 struct MemFactorial {
-    facts: Vec<BigInt>,
+    facts: Option<Vec<BigInt>>,
 }
 impl MemFactorial {
     fn new() -> MemFactorial {
         MemFactorial {
-            facts: vec![BigInt::from(1), BigInt::from(1)],
+            facts: Some(vec![BigInt::from(1), BigInt::from(1)]),
         }
     }
-    fn calc(&mut self, num: usize) -> BigInt {
-        if self.facts.len() > num {
-            return self.facts[num].clone();
+    fn calc(&mut self, num: usize) -> Option<BigInt> {
+        let mut facts = self
+            .facts
+            .take()
+            .unwrap_or(vec![BigInt::from(1), BigInt::from(1)]);
+        if facts.len() > num {
+            return std::panic::catch_unwind(|| facts[num].clone()).ok();
         }
-        for i in self.facts.len()..=num {
-            self.facts
-                .push(self.facts.last().unwrap().clone() * BigInt::from(i));
+        facts.try_reserve(num.saturating_sub(facts.len())).ok()?;
+        self.facts = std::panic::catch_unwind(move || {
+            for i in facts.len()..=num {
+                facts.push(facts.last().unwrap() * BigInt::from(i));
+            }
+            facts
+        })
+        .ok();
+        if let Some(facts) = &self.facts {
+            facts.last().cloned()
+        } else {
+            None
         }
-        self.facts.last().unwrap().clone()
     }
 }
 
-fn multi_factorial(num: usize) -> BigInt {
+fn _multi_factorial(num: usize) -> BigInt {
     if num < 2 {
         return BigInt::from(1);
     }
     (2..=num).into_par_iter().map(BigInt::from).product()
 }
 
+fn simple_factorial(num: usize) -> BigInt {
+    (2..=num).map(BigInt::from).product()
+}
+
+fn multi_factorial(num: usize) -> Option<BigInt> {
+    let cpu_count = num_cpus::get();
+    let prefer = cmp::max(num / 256, 1);
+    let threads_count = cmp::min(cpu_count, prefer);
+    let part = num / threads_count;
+    let mut start = 2usize;
+    let mut threads = Vec::<JoinHandle<BigInt>>::with_capacity(threads_count);
+    for _ in 1..threads_count {
+        threads.push(thread::spawn(move || {
+            let end = start + part;
+            (start..end).map(BigInt::from).product()
+        }));
+        start += part;
+    }
+    threads.push(thread::spawn(move || {
+        (start..=num).map(BigInt::from).product()
+    }));
+    // threads.iter_mut().map(|th| th.join().unwrap()).product()
+    let mut res = BigInt::from(1);
+    for th in threads {
+        if let Ok(cur) = th.join() {
+            res *= cur;
+        } else {
+            return None;
+        }
+    }
+    Some(res)
+}
+
+struct SmartFactorial {
+    mem_factorial: MemFactorial,
+    mem_limit: usize,
+}
+
+impl SmartFactorial {
+    fn new() -> SmartFactorial {
+        SmartFactorial {
+            mem_factorial: MemFactorial::new(),
+            mem_limit: 100_000,
+        }
+    }
+    fn calc(&mut self, num: usize) -> BigInt {
+        if num <= self.mem_limit
+            && let Some(res) = self.mem_factorial.calc(num)
+        {
+            return res;
+        }
+        multi_factorial(num).unwrap_or(simple_factorial(num))
+    }
+}
+
 struct MemCombinations {
-    fact_calc: MemFactorial,
+    smart_factorial: SmartFactorial,
 }
 impl MemCombinations {
     fn new() -> MemCombinations {
         MemCombinations {
-            fact_calc: MemFactorial::new(),
+            smart_factorial: SmartFactorial::new(),
         }
     }
     fn calc(&mut self, base: usize, subset: usize) -> BigInt {
-        if base < 100000 && subset < 100000 {
-            self.fact_calc.calc(base)
-                / self.fact_calc.calc(base - subset)
-                / self.fact_calc.calc(subset)
-        } else {
-            multi_factorial(base) / multi_factorial(base - subset) / multi_factorial(subset)
-        }
+        self.smart_factorial.calc(base)
+            / self.smart_factorial.calc(base - subset)
+            / self.smart_factorial.calc(subset)
     }
 }
 
